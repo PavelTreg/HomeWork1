ab49a8519ce87699a38fc5c492f25e1616665429
PavelTreg <pashat@rambler.ru>
2022-12-01 17:29:39 +0300

    Ready HW2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -3,17 +3,16 @@ import { AffairType } from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
+
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
-
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+     props.deleteAffairCallback(props.affair._id)// need to fix
     }
-
     const nameClass = s.name + ' ' + s2[props.affair.priority]
     const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
     const affairClass = s.affair + ' ' + s2[props.affair.priority]
@@ -24,24 +23,23 @@ function Affair(props: AffairPropsType) {
             className={affairClass}
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+                {props.affair.name}
 
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
-                {/*создаёт студент*/}
+                {props.affair.priority}
 
                 {/**/}
             </div>
-
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
+                onClick={deleteCallback}
                 // need to fix
-
             >
                 {/*текст кнопки могут изменить студенты*/}
-                X
+                Del
                 {/**/}
             </button>
         </div>
